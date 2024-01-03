53f9cda99bdf572e294c815dda4bdd789e553222
Chuck Findlay <chuck@findlayis.me>
2024-01-03 08:07:16 -0400

    Add in rest of colour codes to switch statement

--- main.go
+++ main.go
@@ -52,6 +52,8 @@ func main() {
 				colourSufffix := "\x03"
 				// https://modern.ircdocs.horse/formatting.html
 				switch msg.ColourCode {
+				case 0:
+					colourPrefix = "\x0300"
 				case 1:
 					colourPrefix = "\x0301"
 				case 2:
@@ -60,6 +62,28 @@ func main() {
 					colourPrefix = "\x0303"
 				case 4:
 					colourPrefix = "\x0304"
+				case 5:
+					colourPrefix = "\x0305"
+				case 6:
+					colourPrefix = "\x0306"
+				case 7:
+					colourPrefix = "\x0307"
+				case 8:
+					colourPrefix = "\x0308"
+				case 9:
+					colourPrefix = "\x0309"
+				case 10:
+					colourPrefix = "\x0310"
+				case 11:
+					colourPrefix = "\x0311"
+				case 12:
+					colourPrefix = "\x0312"
+				case 13:
+					colourPrefix = "\x0313"
+				case 14:
+					colourPrefix = "\x0314"
+				case 15:
+					colourPrefix = "\x0315"
 				}
 
 				if colourPrefix != "" {
