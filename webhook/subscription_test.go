1a61865e56a37be446a2ca53f7b14cb590bda841
Chuck Findlay <chuck@findlayis.me>
2024-05-23 10:45:14 -0300

    more testing of subscription

--- webhook/subscription_test.go
+++ webhook/subscription_test.go
@@ -6,14 +6,13 @@ import (
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/cfindlayisme/wmb/database"
 	"github.com/cfindlayisme/wmb/webhook"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSubscribePrivmsg(t *testing.T) {
 	// Create a mock database
 	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("An error '%s' was not expected when opening a stub database connection", err)
-	}
+	require.NoError(t, err, "An error was not expected when opening a stub database connection")
 	defer db.Close()
 
 	// Set the mock database in the DB object
@@ -36,7 +35,55 @@ func TestSubscribePrivmsg(t *testing.T) {
 	webhook.SubscribePrivmsg("target", "url")
 
 	// Make sure all expectations were met
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("There were unfulfilled expectations: %s", err)
-	}
+	require.NoError(t, mock.ExpectationsWereMet(), "There were unfulfilled expectations")
+}
+
+func TestSubscribePrivmsgExistsAndDoesnt(t *testing.T) {
+	// Create a mock database
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err, "An error was not expected when opening a stub database connection")
+	defer db.Close()
+
+	// Set the mock database in the DB object
+	database.DB.SetDB(db)
+
+	// Expectations for the first call
+	mock.ExpectExec("CREATE TABLE IF NOT EXISTS PrivmsgSubscriptions").WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectBegin()
+
+	// Expect the SELECT query
+	mock.ExpectQuery("^SELECT 1 FROM PrivmsgSubscriptions WHERE Target = \\? AND URL = \\?$").
+		WithArgs("target", "url").
+		WillReturnRows(sqlmock.NewRows([]string{"exists"}).AddRow(true))
+
+	// Call the function
+	result := webhook.SubscribePrivmsg("target", "url")
+
+	// Make sure all expectations were met
+	require.NoError(t, mock.ExpectationsWereMet(), "There were unfulfilled expectations")
+
+	// Check the result
+	require.False(t, result, "Expected false")
+
+	// Expectations for the second call
+	mock.ExpectExec("CREATE TABLE IF NOT EXISTS PrivmsgSubscriptions").WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectBegin()
+
+	// Expect the SELECT query
+	mock.ExpectQuery("^SELECT 1 FROM PrivmsgSubscriptions WHERE Target = \\? AND URL = \\?$").
+		WithArgs("target", "url").
+		WillReturnRows(sqlmock.NewRows([]string{"exists"}).AddRow(false))
+
+	mock.ExpectPrepare("INSERT OR IGNORE INTO PrivmsgSubscriptions")
+	mock.ExpectExec("INSERT OR IGNORE INTO PrivmsgSubscriptions").WithArgs("target", "url").WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+
+	// Call the function
+	result = webhook.SubscribePrivmsg("target", "url")
+
+	// Make sure all expectations were met
+	require.NoError(t, mock.ExpectationsWereMet(), "There were unfulfilled expectations")
+
+	// Check the result
+	require.True(t, result, "Expected true")
 }
