efec3582b983925839a46d20d6c2f040ac07904c
Chuck Findlay <chuck@findlayis.me>
2024-12-03 13:41:01 -0400

    Add in some logging for commands for better debugging

--- ircclient/commands.go
+++ ircclient/commands.go
@@ -2,37 +2,44 @@ package ircclient
 
 import (
 	"fmt"
+	"log"
 	"net"
 )
 
 func SetNick(conn net.Conn, nick string) error {
 	_, err := fmt.Fprintf(conn, "NICK "+nick+"\r\n")
+	log.Println("Set nick to ", nick)
 	return err
 }
 
 func JoinChannel(conn net.Conn, channel string) error {
 	_, err := fmt.Fprintf(conn, "JOIN "+channel+"\r\n")
+	log.Println("JOIN command sent for channel: ", channel)
 	return err
 }
 
 func PartChannel(conn net.Conn, channel string) error {
 	_, err := fmt.Fprintf(conn, "PART "+channel+"\r\n")
+	log.Println("PART command sent for channel: ", channel)
 	return err
 }
 
 func SetMode(conn net.Conn, channel string, mode string) error {
 	_, err := fmt.Fprintf(conn, "MODE "+channel+" "+mode+"\r\n")
+	log.Println("MODE command sent for target: ", channel, " with mode: ", mode)
 	return err
 }
 
 func SetTopic(conn net.Conn, channel string, topic string) error {
 	cleanTopic := CleanMessage(topic)
 	_, err := fmt.Fprintf(conn, "TOPIC "+channel+" "+cleanTopic+"\r\n")
+	log.Println("TOPIC command sent for channel: ", channel, " with topic: ", cleanTopic)
 	return err
 }
 
 func InviteUser(conn net.Conn, nick string, channel string) error {
 	_, err := fmt.Fprintf(conn, "INVITE "+nick+" "+channel+"\r\n")
+	log.Println("INVITE command sent for nick: ", nick, " to channel: ", channel)
 	return err
 }
 
